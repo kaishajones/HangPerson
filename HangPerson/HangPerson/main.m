df57b835f38f46f08847b3e34196cc6a7f2bdb67
Kaisha Jones <kaisha.jones@gmail.com>
2015-06-19 21:56:46 -0400

    This is my final HangManPerson

--- HangPerson/HangPerson/main.m
+++ HangPerson/HangPerson/main.m
@@ -11,8 +11,86 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         
-        // code goes here...
-
+        NSLog(@"Hey, Let's play Hang Person");
+        
+        // Secret Word
+        char secretWord[] = "faith";
+        
+        
+        // Current state of guessed word
+        char guessedWord[] = "_____";
+        
+        // Max of wrong attempts
+        int maxNumberOfWrongAttempts = 4;
+        
+        int wrongAttempts = 0;
+        
+        // Max right attempts
+        int maxNumberOfRightAttempts = 5;
+        
+        int rightAttempts = 0;
+        
+        // Begin the game
+        
+        
+        printf("Hint Word: believe \n");
+        // Here's a hint
+        
+        while (true) {
+            
+            // Enter a letter
+            char userGuess;
+            
+            printf("Guess a letter \n\n");
+            scanf("%c", &userGuess);
+            
+            //            printf("%c \n", userGuess);
+            fpurge(stdin);
+            
+            //            int i = userGuess;
+            //            int count = sizeof(secretWord);
+            
+            int match = 0;
+            for (int i = 0; i < strlen(secretWord); i++) {
+                if (userGuess == secretWord[i]) {
+                    guessedWord[i] = userGuess;
+                    match = 1;
+                }
+                //
+                //            }for (int i = 0; i < strlen(guessedWord); i++) {
+                //                  if (userGuess != secretWord[i]) {
+                //                  guessedWord[i] = userGuess;
+                //                  match = 0;
+                //                }
+            }
+            if (match == 0) {
+                printf("\n no match, sorry try again. \n");
+                wrongAttempts++; //
+            }
+            if (match != 0) {
+                printf("\n Great, You have a match!");
+                rightAttempts++;
+                
+                
+                if (wrongAttempts > maxNumberOfWrongAttempts) {
+                    printf("Sorry dude, you lose");
+                    break;
+                }
+                if (rightAttempts == maxNumberOfRightAttempts) {
+                    printf("CONGRATS, YOU WIN!\n");
+                    break;
+                }
+                printf("\n");
+                for (int i = 0; i < strlen(secretWord); i++) {
+                    printf("%c ", guessedWord[i]);
+                }
+                printf("\n");
+                
+                
+            }
+                
+            }
+        
+        return 0;
     }
-    return 0;
 }
